b4cd83b81c78c1bd6a092d5bbc41081f795a65a4
Oscar Fernandez <55814286+Evil004@users.noreply.github.com>
2021-09-24 17:17:42 +0200

    Update ConfigureCore.ps1

--- Fase1-Grupal/ConfigureCore.ps1
+++ Fase1-Grupal/ConfigureCore.ps1
@@ -1,4 +1,13 @@
-#Renombrar el sistema en powershell
+#Renombrar el sistema
 rename-computer master-edu-es-repl
 
-#Cambiar que
+#Obtener la configuracion de la interfaz de red
+Get-NetIPConfiguration
+#Obtener el adaptador de red
+Get-NetAdapter
+
+#Le ponemos la IP 192.168.1.202 en el adaptador de red 4
+New-NetIPAddress -InterfaceIndex 4 -IPaddress 192.168.1.202 -PrefixLength 24 -DefaultGateway 192.168.1.201
+#Le configuramos el DNS
+Set-DnsClientServerAddress -InterfaceIndex 4 -ServerAddresses 192.168.1.201, 192.168.1.202
+
