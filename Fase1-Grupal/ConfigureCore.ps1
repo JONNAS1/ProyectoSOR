ebfe45b6829a8b97dd949c8a47bcf7f4f07cbddc
Oscar Fernandez <55814286+Evil004@users.noreply.github.com>
2021-09-27 19:39:09 +0200

    Update ConfigureCore.ps1

--- Fase1-Grupal/ConfigureCore.ps1
+++ Fase1-Grupal/ConfigureCore.ps1
@@ -1,15 +1,15 @@
-#Renombrar el sistema
+#Oscar: Renombrar el sistema
 rename-computer master-edu-es-repl
 
-#Obtener la configuracion de la interfaz de red
+#Oscar: Obtener la configuracion de la interfaz de red
 Get-NetIPConfiguration
 #Obtener el adaptador de red
 Get-NetAdapter
 
-#Le ponemos la IP 192.168.1.202 en el adaptador de red 4
+#Oscar: Le ponemos la IP 192.168.1.202 en el adaptador de red 4
 New-NetIPAddress -InterfaceIndex 4 -IPaddress 192.168.1.202 -PrefixLength 24 -DefaultGateway 192.168.1.201
 #Le configuramos el DNS
 Set-DnsClientServerAddress -InterfaceIndex 4 -ServerAddresses 192.168.1.201, 192.168.1.202
 
-#Para ver la version del NTFS
+#Oscar: Para ver la version del NTFS
 fsutil fsinfo ntfsinfo C:\
